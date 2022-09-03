0a84ebfe5d2d5fa21854259b92c61113593e9cb3
LilianaFranco <franco0123@gmail.com>
2022-09-02 22:13:24 -0500

    Light Mode for all pages

--- js/lightMode.js
+++ js/lightMode.js
@@ -1,11 +1,22 @@
 
-let goLight = document.querySelector("body")
+let goLight = document.querySelector("#change-mode")
 let navigation = document.querySelectorAll("header a")
+let cards = document.querySelectorAll(".card")
+
 
 //Escuchar el clic de un botón para que funcione el dark mode cuando el usuario desee.
 goLight.addEventListener("click", function (){
-    document.querySelector('body').classList.toggle("light") 
-    navigation.forEach(element => {
+    document.querySelector('body').classList.toggle("light") //cambio de fondo del body
+
+    navigation.forEach(element => { //cambio de color de fuente del menú de navegación
         element.classList.toggle("light") 
     });
+
+    cards.forEach(element => { //cambio del fondo de tarjetas
+        element.classList.toggle("cardMode") 
+    });
+
+    document.querySelector(".contact-form").classList.toggle("formMode")
+    
+
 })
